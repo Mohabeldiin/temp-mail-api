ddd88789ac057d813175faf7a336e688745e4497
Mohab Mohsen <mohabeldiin@gmail.com>
2022-04-24 04:24:57 +0200

    linting

--- TempMailAPI.py
+++ TempMailAPI.py
@@ -1,3 +1,12 @@
+"""Temporary Email API Class
+
+    This class is used to get a temporary email address.
+    It uses the API provided by https://temp-mail.org/
+
+    Example:
+        >>> from temp_mail_api import TempMailAPI
+        >>> api = TempMailAPI()
+        >>> email = api.get_email()"""
 import json
 import logging
 import requests
@@ -14,7 +23,7 @@ class __Email(object):
         """Request for new email
         Return:
             email: str of email as Mail@Domain
-            Mail: str of Mail 
+            Mail: str of Mail
             Domain: str of Domain"""
         super().__init__()
         logging.basicConfig(
@@ -23,7 +32,7 @@ class __Email(object):
         self._logger.info("Initializing TempMail")
         self._api = "https://www.1secmail.com/api/v1/?"
         self._email, self._mail, self._domain = self._creat_new_email()
-        self._logger.info(f"TempMail initialized with : {self._email}")
+        self._logger.info("TempMail initialized with : %s", self._email)
 
     def _creat_new_email(self):
         """Request for new email
@@ -31,14 +40,14 @@ class __Email(object):
             email: str of email as Mail@Domain
             Mail: str of Mail
             Domain: str of Domain"""
-        self._logger.info(f"Creating new email")
+        self._logger.info("Creating new email")
         action = "genRandomMailbox"
         count = 1
         new_mail_request = f"{self._api}action={action}&count={count}"
         new_mail_response = self._request_handler(new_mail_request)
         email = self.__extract_email(new_mail_response)
         mail, domain = email.split("@")
-        self._logger.info(f"New email created: {email}")
+        self._logger.info("New email created: %s", email)
         return email, mail, domain
 
     def __extract_email(self, response):
@@ -51,7 +60,7 @@ class __Email(object):
         for symbol in email:
             if symbol == "[" or symbol == "\"" or symbol == "]":
                 email = email.replace(symbol, "")
-        self._logger.info(f"Extracted email: {email}")
+        self._logger.info("Extracted email: %s", email)
         return email
 
     def __try(self, request, exception, count=3):
@@ -67,15 +76,15 @@ class __Email(object):
             exception: Exception"""
         if count:
             try:
-                self._logger.debug(f"Retrying request: {request}")
+                self._logger.debug("Retrying request: %s", request)
                 return requests.get(request)
-            except exception:
-                self._logger.error(f"Exception: {exception}")
-                self._logger.debug(f"Retrying request: {request}")
-                return self.__try(request, exception, count-1)
+            except exception as ex:
+                self._logger.error("Exception: %s", ex.__doc__)
+                self._logger.debug("Retrying request: %s", request)
+                return self.__try(request, ex, count-1)
         else:
-            self._logger.error(f"Exception: {exception}")
-            self._logger.critical(f"Request failed: {request}")
+            self._logger.error("Exception: %s", exception)
+            self._logger.critical("Request failed: %s", request)
             raise exception
 
     def _request_handler(self, request):
@@ -88,31 +97,37 @@ class __Email(object):
         Raises:
             exception: Exception"""
         try:
-            self._logger.debug(f"Requesting: {request}")
+            self._logger.debug("Requesting: %s", request)
             return requests.get(request)
         except requests.exceptions.ConnectionError as ex:
-            self._logger.debug(f"ConnectionError: {ex}")
+            self._logger.debug("ConnectionError: %s", ex.__doc__)
             return self.__try(request, ex)
         except requests.exceptions.Timeout as ex:
-            self._logger.debug(f"Timeout: {ex}")
+            self._logger.debug("Timeout: %s", ex.__doc__)
             return self.__try(request, ex)
         except requests.exceptions.TooManyRedirects as ex:
-            self._logger.debug(f"TooManyRedirects: {ex}")
+            self._logger.debug("TooManyRedirects: %s", ex.__doc__)
             return self.__try(request, ex)
 
     def get_mail(self):
-        """foo"""
-        self._logger.info(f"Returning mail: {self._mail}")
+        """Get Mail
+        Return:
+            mail: str of Mail"""
+        self._logger.info("Returning mail: %s", self._mail)
         return self._mail
 
     def get_domain(self):
-        """foo"""
-        self._logger.info(f"Returning domain: {self._domain}")
+        """Get Domain
+        Return:
+            domain: str of Domain"""
+        self._logger.info("Returning domain: %s", self._domain)
         return self._domain
 
     def get_email(self):
-        """foo"""
-        self._logger.info(f"Returning email: {self._email}")
+        """Get Email
+        Return:
+            email: str of email as Mail@Domain"""
+        self._logger.info("Returning email: %s", self._email)
         return self._email
 
 
@@ -144,9 +159,9 @@ class TempMail(__Email):
         Return:
             message: str of message"""
         try:
-            self._logger.debug(f"Handling json data: {data}")
+            self._logger.debug("Handling json data: %s", data)
             return json.loads(data)
-        except:
+        except: # pylint: disable=bare-except
             self._logger.error("Message not found")
             return {}
 
@@ -154,17 +169,17 @@ class TempMail(__Email):
         """Handling the data extraction"""
         try:
             return data[0][attribute].strip()
-        except:
+        except: # pylint: disable=bare-except
             try:
                 return data[attribute]
             except IndexError as ex:
-                self._logger.error(f"No attribute found: {ex}", exc_info=True)
+                self._logger.error("No attribute found: %s", ex.__doc__)
                 return []
             except TypeError as ex:
-                self._logger.error(f"No attribute found: {ex}", exc_info=True)
+                self._logger.error("No attribute found: %s", ex.__doc__)
                 return []
             except KeyError as ex:
-                self._logger.error(f"No attribute found: {ex}", exc_info=True)
+                self._logger.error("No attribute found: %s", ex.__doc__)
                 return []
 
     def __last_received_mail(self):
@@ -173,10 +188,10 @@ class TempMail(__Email):
             response: requests.Response"""
         action = "getMessages"
         mail_request = f"{self._api}action={action}&login={self._mail}&domain={self._domain}"
-        self._logger.debug(f"Requesting: {mail_request}")
+        self._logger.debug("Requesting: %s", mail_request)
         mail_response = self._request_handler(mail_request)
         self._logger.debug(
-            f"Returning last received mail: {mail_response.text}")
+            "Returning last received mail: %s", mail_response.text)
         return self.__json_handler(mail_response.text)
 
     def __last_received_id(self):
@@ -184,20 +199,20 @@ class TempMail(__Email):
         Return:
             id: int of id"""
         mail = self.__last_received_mail()
-        self._logger.debug(f"Returning last received id.")
+        self._logger.debug("Returning last received id.")
         return self.__data_handler(mail, "id")
 
     def __last_received_message(self):
         """Request for the last received message
         Return:
             message: str of message"""
-        id = self.__last_received_id()
+        message_id = self.__last_received_id()
         action = "readMessage"
-        message_request = f"{self._api}action={action}&login={self._mail}&domain={self._domain}&id={id}"
-        self._logger.debug(f"Requesting: {message_request}")
+        message_request = f"{self._api}action={action}&login={self._mail}&domain={self._domain}&id={message_id}"
+        self._logger.debug("Requesting: %s", message_request)
         message_response = self._request_handler(message_request)
         self._logger.debug(
-            f"Returning last received message: {message_response.text}")
+            "Returning last received message: %s", message_response.text)
         return self.__json_handler(message_response.text)
 
     def __last_received_subject(self):
@@ -205,7 +220,7 @@ class TempMail(__Email):
         Return:
             subject: str of subject"""
         message = self.__last_received_message()
-        self._logger.debug(f"Returning last received subject.")
+        self._logger.debug("Returning last received subject.")
         return self.__data_handler(message, "subject")
 
     def __last_received_body(self):
@@ -213,33 +228,35 @@ class TempMail(__Email):
         Return:
             body: str of body"""
         message = self.__last_received_message()
-        self._logger.debug(f"Returning last received body.")
+        self._logger.debug("Returning last received body.")
         return str(self.__data_handler(message, "textBody"))
 
     def get_subject(self):
         """Gets the last received subject
         Return:
             subject: str of subject"""
-        self._logger.info(
-            f"Returning subject: {self.__last_received_subject()}")
+        self._logger.info("Returning subject: %s",
+                          self.__last_received_subject())
         return self.__last_received_subject()
 
     def get_message(self):
         """Gets the last received body
         Return:
             body: str of body"""
-        self._logger.info(f"Returning body: {self.__last_received_body()}")
+        self._logger.info("Returning body: %s", self.__last_received_body())
         return self.__last_received_body()
 
     def receive_mail(self):
-        """foo"""
+        """Receive the last received mail
+        Return:
+            mail: dict of mail"""
         subject = self.__last_received_subject()
         body = self.__last_received_body()
         received_mail = {
             "subject": subject,
             "body": str(body)
         }
-        self._logger.info(f"Returning received mail: {received_mail}")
+        self._logger.info("Returning received mail: %s", received_mail)
         return received_mail
 
 
